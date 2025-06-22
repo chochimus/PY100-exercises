ecff9094a8c98dbdd4b3d9c54f4338ce88612883
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-22 17:24:39 -0500

    Missing space

--- reading_documentation2/ex5.py
+++ reading_documentation2/ex5.py
@@ -5,5 +5,5 @@ today = date.today()
 today_year = today.year
 # .year is a read only instance attribute of a date object
 iso_year = today.isocalendar()[0]
-# .isocalendar returns a tuple that has a year component.
+# .isocalendar returns a tuple that has an ISO year component.
 # ISO is a variation of the Gregorian calendar
