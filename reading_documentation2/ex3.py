1ae6ca03dd0be4d07cc5fd951c4d455a119549ad
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-21 22:04:36 -0500

    Completed exercise 3

---
+++ reading_documentation2/ex3.py
@@ -0,0 +1,5 @@
+to_print = 4 * 5 + 3**2 / 10
+
+# from documentation ** has highest precedence followed by *, /, +
+# this means that the order will be ((4 * 5) + ((3**2) / 10))
+print(to_print) # 20.9
