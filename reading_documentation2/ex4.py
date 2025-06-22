2cf1ce1ac9806942b64a46a0c43cc116e4619bde
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-22 17:04:54 -0500

    Completed exercise 4

---
+++ reading_documentation2/ex4.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+# https://docs.python.org/3/library/datetime.html#datetime.datetime.today
+# the current date and time can be obtained using the now method 
+print(datetime.now())
