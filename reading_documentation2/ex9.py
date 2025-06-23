6fc1e07f272ffd56118223761bd3d849a2856e63
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-23 13:27:27 -0500

    Completed exercise 9

---
+++ reading_documentation2/ex9.py
@@ -0,0 +1,11 @@
+tweet = 'Woohoo! :-)'
+
+if len(tweet) > 140:
+    print('Tweet is too long!')
+
+# length_of_tweet = len(tweet + 5)
+length_of_tweet = len(tweet) + 5
+print(length_of_tweet)
+
+# A type error is raised when an operation or function is applied
+# an object of inappropriate type.
