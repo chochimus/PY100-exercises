95fe93bbc1e950d97aa639d7129ec52dab010488
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-23 19:32:19 -0500

    Completed exercise 8

---
+++ Conditionals/ex8.py
@@ -0,0 +1,8 @@
+if True and False:
+    print('Yes!')
+else:
+    print('No...')
+
+# IDE hints to solution like previous example but this will always
+# print No... True and False evaluates to False so the if statement
+# will never be entered
