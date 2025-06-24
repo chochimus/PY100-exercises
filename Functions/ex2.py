d3fe62036603ee30d1e7c5b7a536b0b6ee7184f3
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-23 20:49:46 -0500

    Completed exercise 2

---
+++ Functions/ex2.py
@@ -0,0 +1,8 @@
+def bruce_eckel_quote():
+    print('Python is executable pseudocode.')
+
+# Functions that return no value return None
+print(bruce_eckel_quote())
+
+# Calling the function in a print, the function runs (printing the quote)
+# then the return value is printed (None)
