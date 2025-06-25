6dacc38df0f0022158321891371c395b62b97b8f
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-24 20:51:59 -0500

    Completed exercise 7

---
+++ Lists/ex7.py
@@ -0,0 +1,8 @@
+list1 = [2, 6, 4]
+list2 = [2, 6, 4]
+
+print(list1 == list2)
+
+# should print true, the values are equal
+# if however we test is we will get false as they are different
+# objects
