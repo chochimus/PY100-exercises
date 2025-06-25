ede3e774212f42212164bae9b5ea6dc200cd13b3
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-24 20:37:24 -0500

    Completed exercise 6

---
+++ Lists/ex6.py
@@ -0,0 +1,17 @@
+vocabulary = [
+    ['happy', 'cheerful', 'merry', 'glad'],
+    ['tired', 'sleepy', 'fatigued', 'drained'],
+    ['excited', 'eager', 'enthused', 'animated'],
+]
+
+# happy
+# cheerful
+# merry
+# glad
+# tired
+# sleepy
+# etc...
+
+for line in vocabulary:
+    for word in line:
+        print(word)
